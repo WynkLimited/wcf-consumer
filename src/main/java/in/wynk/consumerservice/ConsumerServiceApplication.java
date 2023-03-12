549ada134adbef4fb9f0daa5aee227e3cdf4d822
Sharad Gupta Wynk <sharad1.gupta@wynk.in>
2023-03-12 12:48:56 +0530

    aws secret manager config

--- src/main/java/in/wynk/consumerservice/ConsumerServiceApplication.java
+++ src/main/java/in/wynk/consumerservice/ConsumerServiceApplication.java
@@ -3,9 +3,11 @@ package in.wynk.consumerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
+@ComponentScan("in.wynk.*")
 public class ConsumerServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/in/wynk/consumerservice/service/WcfCallbackService.java
+++ src/main/java/in/wynk/consumerservice/service/WcfCallbackService.java
@@ -17,6 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -46,6 +48,12 @@ public class WcfCallbackService {
 
     @Autowired
     private WcfUtils wcfUtils;
+
+    @Autowired
+    private Environment env;
+
+    @Value("${wynk/wcf-consumer/staging}")
+    private String scyllaPassword;
 
     @AnalyseTransaction(name = "wcfCallback")
     public String handleCallback(SubscriptionEvent subscriptionEvent) {
